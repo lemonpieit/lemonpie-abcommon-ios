4028ef0a9f59279d30ddc6822f004caf7950879c
Francesco Leoni <francesco.leoni@abenergie.it>
2020-05-18 14:38:07 +0200

    Added anchors extension

--- Source/Extensions/UIView+XT.swift
+++ Source/Extensions/UIView+XT.swift
@@ -86,4 +86,94 @@ public extension UIView {
         
         return blurEffectView
     }
+    
+    // Anchors
+    struct AnchoredConstraints {
+        var top, leading, bottom, trailing, width, height: NSLayoutConstraint?
+    }
+
+    @discardableResult
+    func anchor(top: NSLayoutYAxisAnchor? = nil, leading: NSLayoutXAxisAnchor? = nil, bottom: NSLayoutYAxisAnchor? = nil, trailing: NSLayoutXAxisAnchor? = nil, padding: UIEdgeInsets = .zero, size: CGSize = .zero) -> AnchoredConstraints {
+        translatesAutoresizingMaskIntoConstraints = false
+        var anchoredConstraints = AnchoredConstraints()
+        
+        if let top = top { anchoredConstraints.top = topAnchor.constraint(equalTo: top, constant: padding.top) }
+        
+        if let leading = leading { anchoredConstraints.leading = leadingAnchor.constraint(equalTo: leading, constant: padding.left) }
+        
+        if let bottom = bottom { anchoredConstraints.bottom = bottomAnchor.constraint(equalTo: bottom, constant: -padding.bottom) }
+        
+        if let trailing = trailing { anchoredConstraints.trailing = trailingAnchor.constraint(equalTo: trailing, constant: -padding.right) }
+        
+        if size.width != 0 { anchoredConstraints.width = widthAnchor.constraint(equalToConstant: size.width) }
+        
+        if size.height != 0 { anchoredConstraints.height = heightAnchor.constraint(equalToConstant: size.height) }
+        
+        [anchoredConstraints.top, anchoredConstraints.leading, anchoredConstraints.bottom, anchoredConstraints.trailing, anchoredConstraints.width, anchoredConstraints.height].forEach{ $0?.isActive = true }
+        
+        return anchoredConstraints
+    }
+    
+    @discardableResult
+    func fillSuperview(padding: UIEdgeInsets = .zero) -> AnchoredConstraints {
+        translatesAutoresizingMaskIntoConstraints = false
+        let anchoredConstraints = AnchoredConstraints()
+        
+        guard let superviewTopAnchor = superview?.topAnchor,
+            let superviewBottomAnchor = superview?.bottomAnchor,
+            let superviewLeadingAnchor = superview?.leadingAnchor,
+            let superviewTrailingAnchor = superview?.trailingAnchor else {
+                return anchoredConstraints
+        }
+        
+        return anchor(top: superviewTopAnchor, leading: superviewLeadingAnchor, bottom: superviewBottomAnchor, trailing: superviewTrailingAnchor, padding: padding)
+    }
+    
+    @discardableResult
+    func fillSuperviewSafeAreaLayoutGuide(padding: UIEdgeInsets = .zero) -> AnchoredConstraints {
+        let anchoredConstraints = AnchoredConstraints()
+        
+        guard let superviewTopAnchor = superview?.safeAreaLayoutGuide.topAnchor,
+            let superviewBottomAnchor = superview?.safeAreaLayoutGuide.bottomAnchor,
+            let superviewLeadingAnchor = superview?.safeAreaLayoutGuide.leadingAnchor,
+            let superviewTrailingAnchor = superview?.safeAreaLayoutGuide.trailingAnchor else {
+                return anchoredConstraints
+        }
+        return anchor(top: superviewTopAnchor, leading: superviewLeadingAnchor, bottom: superviewBottomAnchor, trailing: superviewTrailingAnchor, padding: padding)
+    }
+    
+    func centerToSuperview() {
+        translatesAutoresizingMaskIntoConstraints = false
+        if let superviewCenterXAnchor = superview?.centerXAnchor {
+            centerXAnchor.constraint(equalTo: superviewCenterXAnchor).isActive = true
+        }
+        
+        if let superviewCenterYAnchor = superview?.centerYAnchor {
+            centerYAnchor.constraint(equalTo: superviewCenterYAnchor).isActive = true
+        }
+    }
+    
+    func centerXTo(_ anchor: NSLayoutXAxisAnchor) {
+        translatesAutoresizingMaskIntoConstraints = false
+        centerXAnchor.constraint(equalTo: anchor).isActive = true
+    }
+    
+    func centerYTo(_ anchor: NSLayoutYAxisAnchor) {
+        translatesAutoresizingMaskIntoConstraints = false
+        centerYAnchor.constraint(equalTo: anchor).isActive = true
+    }
+    
+    func centerXToSuperview() {
+        translatesAutoresizingMaskIntoConstraints = false
+        if let superviewCenterXAnchor = superview?.centerXAnchor {
+            centerXAnchor.constraint(equalTo: superviewCenterXAnchor).isActive = true
+        }
+    }
+    
+    func centerYToSuperview() {
+        translatesAutoresizingMaskIntoConstraints = false
+        if let superviewCenterYAnchor = superview?.centerYAnchor {
+            centerYAnchor.constraint(equalTo: superviewCenterYAnchor).isActive = true
+        }
+    }
 }
